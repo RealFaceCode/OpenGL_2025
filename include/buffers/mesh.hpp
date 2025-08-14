93c208b3278f73546c75c46bdc37e892a1c98da3
RealFaceCode <kevin-ernst@xyz.de>
2025-08-14 23:12:29 +0200

    Refactor mesh.hpp to improve formatting and add type aliases for Mesh2D and Mesh3D

--- include/buffers/mesh.hpp
+++ include/buffers/mesh.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
-#include<glad/gl.h>
+#include <glad/gl.h>
 #include <vector>
 
+struct Vertex2D;
+struct Vertex3D;
 
 template<class VertexType>
+requires std::is_same_v<VertexType, Vertex2D> || std::is_same_v<VertexType, Vertex3D>
 class Mesh
 {
 public:
@@ -24,3 +27,5 @@ private:
     std::vector<VertexType> vertices;
     std::vector<GLuint> indices;
 };
+using Mesh2D = Mesh<Vertex2D>;
+using Mesh3D = Mesh<Vertex3D>;
